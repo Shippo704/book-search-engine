ebd56ecabb6c842c5c4d54cfc8a83a402add0096
Shippo704 <matt.taylor704@yahoo.ca>
2023-10-08 11:24:51 -0400

    added user-routes to typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,7 +2,7 @@
 const {gql} = require('apollo-server-express');
 
 const typeDefs = gql `
-    // models
+
     type Book {
         bookId: ID!
         authors: [String]
@@ -21,7 +21,30 @@ const typeDefs = gql `
         savedBooks: [Book]
     }
 
-    
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    input BookInput {
+        bookId: String!
+        authors: [String]
+        description: String
+        image: String
+        link: String
+        title: String!
+    }
+
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+        saveBook: (bookData: BookInput!): User
+        removeBook(bookId: ID!): User
+    }
 
 
 `
