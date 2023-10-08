d3c268967a4cc69b1d0805a34a52748542bfd128
Shippo704 <matt.taylor704@yahoo.ca>
2023-10-08 11:59:44 -0400

    added server/schemas/index.js

---
+++ server/schemas/index.js
@@ -0,0 +1,6 @@
+// import required modules
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+
+// export to higher level as objects
+module.exports = { typeDefs, resolvers };
